c253ec8afd257855c47302d82f6f2104d55eaf73
Yash Lotan <yashlotan7@gmail.com>
2019-01-19 11:59:17 +0530

    Duplicate account check

--- parseAccountList.js
+++ parseAccountList.js
@@ -15,21 +15,33 @@ fs.readFile("./input.txt", (err, data) => {
 	data = data.toString();
 
 	data.split("\n").forEach(a => {
-		accpw = a.trim().split(":");
+    		accpw = a.trim().split(":");
+    		
+		//Check if account is already present in our list
+    		var repeated = 0;
+    		for (var i = 0; i < accounts.length; i++) {
+      			if (accounts[i]["username"] === accpw[0]) {
+        			repeated = 1;
+			        break;
+      			}
+    		}
 
-		console.log(accpw[0]);
-
-		accounts.push({
-			username: accpw[0],
-			password: accpw[1],
-			sharedSecret: "",
-			operational: true,
-			lastCommend: -1,
-			lastReport: -1,
-			lastServerReport: -1,
-			requiresSteamGuard: false,
-			commended: []
-		});
+    		console.log(accpw[0]);
+    		if (!repeated) {
+      			accounts.push({
+        			username: accpw[0],
+        			password: accpw[1],
+        			sharedSecret: "",
+        			operational: true,
+        			lastCommend: -1,
+        			lastReport: -1,
+        			lastServerReport: -1,
+        			requiresSteamGuard: false,
+        			commended: []
+      			});
+    		} else {
+      			console.log("Account already present : " + accpw[0]);
+    		}
 	});
 
 	fs.writeFile("./accounts.json", JSON.stringify(accounts, null, 4), (err) => {
